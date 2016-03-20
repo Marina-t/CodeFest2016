260d133b9e43865994212b22078e99a1d71251db
Marina Tawfik <>
2016-03-20 18:25:42 -0400

    added the completed code.

--- Script/HangManGame.js
+++ Script/HangManGame.js
@@ -15,8 +15,11 @@ function StartGame () {
 	 *[Task 6]: Complete the startGame function
      */
 
+
 	// 1. initialize the number of lives, word, charactersToGuess 
-	
+	lives = 6;
+	word = "";
+	charactersToGuess = 0;
 
 	// 2. pick word and display the correct number of spaces accordingly
 	ChooseWord();
@@ -25,7 +28,8 @@ function StartGame () {
 	PopulateAlphabet();
 
 	// 4. re-set the final result div (empty the div)
-	
+	$("#final-result").empty();
+
 
 	// 5. set up the hangman div
 	$("#hangman").empty();
@@ -48,13 +52,26 @@ function ChooseWord () {
 
 	// 1. randomly pick a word from the avilable list of words 
 	//    and set the gloabal variable word to that chosen word
-	
+	var randomIndex = Math.floor(Math.random() * Words.length);
+	word = Words[randomIndex];
 
 	// 2. empty the word div, iterate over the characters in the word and for each
 	//    a. if it is space --> add a new div with class word-character (for styling)
 	//    b. if it is not --> add a new div with class word-character and class place-holder 
 	//                        also you would need to increment characters to guess
+	$("#word").empty();
+	var newLetter;
+	for (var i = 0; i < word.length; i++ ) {
+		if (word[i] == " ") {
+			newLetter = $("<div></div>").addClass("word-character"); 
+		} 
+		else {
+			newLetter = $("<div></div>").addClass("word-character").addClass("place-holder");
+			charactersToGuess++;
+		}
 
+		$("#word").append(newLetter);
+	}
 	
 }
 
@@ -67,18 +84,25 @@ function PopulateAlphabet () {
 	 *[Task 8]: Complete the PopulateAlphabet function
      */
 
-	// 1. create the alphabet list (use a ul element)
-	
+	// 1. create the alphabet-list (use a ul element)
+	var alphabetList = $("<ul id='alphabet-list'></ul>");
 
 	// 2. go over each letter in the alphabet list and for each:
 	//	a. create a li with id = the letter
 	//  b. add class letter to the element and class active (for styling)
 	//  c. attach an event to the letter so when it is clicked, Check Guess function is called
 	//  d. append the current letter to the element (so the user would see it)
-	
+	var newLetter;
+	for (var i = 0; i < Alphabet.length; i++) {
+		newLetter = $("<li id='" + Alphabet[i].toLowerCase() + "'></li>").addClass("letter");
+		newLetter.addClass("active").on("click", CheckGuess).text(Alphabet[i]);
+
+		alphabetList.append(newLetter);
+	}
 	
 
     // 3. empty the alphabet div and attach the list you created to it
+    $("#alphabet").append(alphabetList);
 }
 
 /* 
@@ -117,6 +141,35 @@ function DrawStickMan () {
 	/* 
 	 * [Task 9]: Using a switch statements, call the appropriate DrawStickMan function
 	 */
+	switch (lives) {
+
+		case 6 :
+			DrawStickManHead();
+			break;
+
+		case 5:
+			DrawStickManTorso();
+			break;
+
+		case 4:
+			DrawStickManRarm();
+			break;
+
+		case 3:
+			DrawStickManLarm();
+			break;
+
+		case 2:
+			DrawStickManRleg();
+			break;
+
+		case 1:
+			DrawStickManLleg();
+			break;
+
+		default:
+			break;
+	}
 	
 }
 
@@ -157,14 +210,14 @@ function CheckGameEnded() {
 	if (lives == 0) {
 		// 1. make the final result's html be a <p> element with message:
 		//    GAME OVER
-		
+		$("#final-result").html("<p>GAME OVER</p>");
 
 		gameEnded = true;
 	}
 	else if (charactersToGuess == 0) {
 		// 2. make the final result's html be a <p> element with message:
 		//    YEAAAAY!!! YOU WON
-
+		$("#final-result").html("<p>YEAAAAY!!! YOU WON</p>");
 		gameEnded = true;
 	}
 
@@ -178,3 +231,10 @@ function CheckGameEnded() {
 /* [Task 5]: attach the StartGame function to both when the 
  * document is done loading and when the user clicks the play again button
  */ 
+$(document).ready(function() {
+
+	$("#play-again > button").on("click", StartGame);
+
+	StartGame();
+
+});
